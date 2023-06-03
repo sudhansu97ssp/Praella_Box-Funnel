337758e9bf82d88ddb1762bdbad25245aafa7288
sudhansu-shiryam <sudhansu.pati@shiryam.com>
2023-06-03 20:04:17 +0530

    navigation functionality added

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function handleClick(element) {
 
   localStorage.setItem("selectedImage", imageSrc);
   localStorage.setItem("imageHeading", divText1);
-  window.location.href = "bagSelection.html";
+  window.location.href = "bag.html";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -16,8 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const selectedImage = localStorage.getItem("selectedImage");
   const imageHeading = localStorage.getItem("imageHeading");
   if (selectedImage && imageHeading) {
-    finalImage.setAttribute("src", selectedImage);
-    imgHeading.textContent = imageHeading;
+    finalImage?.setAttribute("src", selectedImage);
+    if (imgHeading) {
+      imgHeading.textContent = imageHeading;
+    }
+
     // Clear the stored image URL from localStorage
     //   localStorage.removeItem('selectedImage');
   }
@@ -67,8 +70,9 @@ for (let i = 0; i < numOfCards; i++) {
   imageCard.appendChild(subNameElement);
   imageCard.appendChild(counter);
 
-  imageCardsContainer.appendChild(imageCard);
+  imageCardsContainer?.appendChild(imageCard);
 }
+
 
 function decreaseQuantity() {
   const quantityElement = this.nextElementSibling;
@@ -89,17 +93,82 @@ function handleClickQuantity(element) {
   const quantityElement = element.querySelector(".imgCardText");
 
   let selectedQuantity = parseInt(quantityElement.textContent, 10);
-  localStorage.setItem("quantity", selectedQuantity)
+  localStorage.setItem("quantity", selectedQuantity);
   window.location.href = "roast.html";
 }
 
+
 document.addEventListener("DOMContentLoaded", function () {
   const productQuantity = document.querySelector(".product_quantity");
-  
+
   const totalQuantity = localStorage.getItem("quantity");
-  
+
   productQuantity.textContent = `You have selected ${""}/${totalQuantity}`;
 });
 
 
 
+
+function highlightNavigation() {
+  // Get the current URL and extract the HTML file name
+  var currentURL = window.location.href;
+
+  var fileName = currentURL.substring(
+    currentURL.lastIndexOf("/") + 1,
+    currentURL.lastIndexOf(".")
+  );
+
+  // Get the navigation container element
+  var navigationContainer = document.querySelector(".navigation_container");
+
+  // Get all the screen elements within the navigation container
+  var screenElements = navigationContainer.querySelectorAll(".screen");
+
+  // Loop through each screen element and apply the appropriate styles
+  for (var i = 0; i < screenElements.length; i++) {
+    var screenElement = screenElements[i];
+
+    var screenText = screenElement.textContent.trim().toLowerCase();
+
+    if (screenText.includes(fileName) || screenText===fileName) {
+      // Match found
+      
+      screenElement.style.fontWeight = "bold";
+      screenElement.style.color = "#9D1C30";
+
+      // Array of previous screen elements
+      var prevScreenElements = [];
+      if (i > 0) {
+        for (var j = i - 1; j >= 0; j--) {
+          prevScreenElements.push(screenElements[j]);
+        }
+      }
+
+      // Array of next screen elements
+      var nextScreenElements = [];
+      if (i < screenElements.length - 1) {
+        for (var k = i + 1; k < screenElements.length; k++) {
+          nextScreenElements.push(screenElements[k]);
+        }
+      }
+
+      // Apply styles to previous screen elements
+      for (var p = 0; p < prevScreenElements.length; p++) {
+        var prevScreenElement = prevScreenElements[p];
+        prevScreenElement.style.fontWeight = "bold";
+        prevScreenElement.style.color = "black";
+      }
+
+      // Apply styles to next screen elements
+      for (var n = 0; n < nextScreenElements.length; n++) {
+        var nextScreenElement = nextScreenElements[n];
+        nextScreenElement.style.fontWeight = "bold";
+        nextScreenElement.style.opacity = "0.3";
+      }
+    } 
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  highlightNavigation();
+});
